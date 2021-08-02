c6f4b877ee96177fdba9a47ebc815f811a6bfb6e
Nicolas <nicolasdiarra777@gmail.com>
2021-08-02 23:45:09 +0200

    Add oauth connection google&&facebook

--- src/app/feature/login/login.component.ts
+++ src/app/feature/login/login.component.ts
@@ -1,22 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FacebookLoginProvider,
+  GoogleLoginProvider,
+  SocialAuthService,
+  SocialUser,
+} from 'angularx-social-login';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.sass'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup | any;
+  socialUser: SocialUser | any;
+  isLoggedin: boolean = false;
 
-  username: any
-  prout: any = [{caca: 1}, {caca:2}]
-  info = 'info'
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private socialAuthService: SocialAuthService
+  ) {
+    console.log(this.isLoggedin);
   }
 
-  connect () {
-// console.log('data', data.value)
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+
+    this.socialAuthService.authState.subscribe((user) => {
+      this.socialUser = user;
+      this.isLoggedin = user != null;
+      console.log(this.socialUser);
+    });
   }
 
+  loginWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  loginWithFacebook() {
+    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  logOut(): void {
+    this.socialAuthService.signOut();
+  }
+
+  signOut(): void {
+    this.socialAuthService.signOut();
+  }
 }
